f910e94329596d336e4bc5aacd9a7078427e6a12
Scott Hilbert <github@shilbert.com>
2013-02-07 15:24:41 -0500

    Reduced RAM usage of RX mode & got it working again with moved SS pin

--- NordicRX/NordicRX.ino
+++ NordicRX/NordicRX.ino
@@ -1,9 +1,8 @@
 #include <SPI.h>
+#include <avr/pgmspace.h>
 
-#define PIN_CS 7
 #define PIN_CE 8
 #define PIN_IRQ 9
-
 #define PIN_SS 10
 #define PIN_MOSI 11
 #define PIN_MISO 12
@@ -36,12 +35,29 @@
 #define REG_DYNPD       0x1C
 #define REG_FEATURE     0x1D
 
+void print_p(const char* pstr)
+{
+  while(pgm_read_byte(pstr)) {
+    Serial.write(pgm_read_byte(pstr));
+    ++pstr;
+  }
+}
+  
+void println_p(const char* pstr)
+{
+  print_p(pstr);
+  print_p(PSTR("\r\n"));
+}
+
+#define PRINT_P(__str) print_p(PSTR(__str))
+#define PRINTLN_P(__str) println_p(PSTR(__str))
+
 void dump_regs(uint8_t pin);
 
 void readReg(uint8_t csPin, uint8_t addr, uint8_t length, uint8_t* data)
 {
-  digitalWrite(csPin, LOW);
   SPI.begin();
+  digitalWrite(csPin, LOW);
   SPI.transfer(addr & 0x1F);
   while(length > 0)
   {
@@ -60,8 +76,8 @@ void writeReg(uint8_t csPin, uint8_t addr, uint8_t data)
 
 void writeReg(uint8_t csPin, uint8_t addr, uint8_t length, uint8_t* data)
 {
-  digitalWrite(csPin, LOW);
   SPI.begin();
+  digitalWrite(csPin, LOW);
   SPI.transfer(0x20 | (addr & 0x1F));
   while(length > 0)
   {
@@ -75,8 +91,8 @@ void writeReg(uint8_t csPin, uint8_t addr, uint8_t length, uint8_t* data)
 
 void transmit(uint8_t csPin, uint8_t cePin, uint8_t length, uint8_t* data)
 {
-  digitalWrite(csPin, LOW);
   SPI.begin();
+  digitalWrite(csPin, LOW);
   SPI.transfer(0xA0);
   while(length > 0)
   {
@@ -97,8 +113,8 @@ void receive(uint8_t csPin, uint8_t cePin, uint8_t length, uint8_t* data)
 {
   digitalWrite(cePin, LOW);
   
-  digitalWrite(csPin, LOW);
   SPI.begin();
+  digitalWrite(csPin, LOW);
   SPI.transfer(0x61);
   while(length > 0)
   {
@@ -116,16 +132,16 @@ void receive(uint8_t csPin, uint8_t cePin, uint8_t length, uint8_t* data)
 
 void flushTX(uint8_t csPin)
 {
-  digitalWrite(csPin, LOW);
   SPI.begin();
+  digitalWrite(csPin, LOW);
   SPI.transfer(0xE1);
   digitalWrite(csPin, HIGH);
 }
 
 void flushRX(uint8_t csPin)
 {
-  digitalWrite(csPin, LOW);
   SPI.begin();
+  digitalWrite(csPin, LOW);
   SPI.transfer(0xE2);
   digitalWrite(csPin, HIGH);
 }
@@ -150,23 +166,23 @@ void initRX(uint8_t csPin)
 
 void dumpStatus(uint8_t statusReg)
 {
-  Serial.print("Status: ");
+  PRINT_P("Status: ");
   if(statusReg & (1<<6))
-    Serial.print("RX_DR; ");
+    PRINT_P("RX_DR; ");
   if(statusReg & (1<<5))
-    Serial.print("TX_DS; ");
+    PRINT_P("TX_DS; ");
   if(statusReg & (1<<4))
-    Serial.print("MAX_RT; ");
+    PRINT_P("MAX_RT; ");
   if(statusReg & (1<<0))
-    Serial.print("TX_FULL; ");
+    PRINT_P("TX_FULL; ");
   uint8_t rxPipeNo = (statusReg >> 1) & 0x7;
   if(rxPipeNo == 0x7)
-    Serial.print("RX FIFO empty");
+    PRINT_P("RX FIFO empty");
   else
   {
-    Serial.print("RX FIFO ");
+    PRINT_P("RX FIFO ");
     Serial.print(rxPipeNo, DEC);
-    Serial.print(" contains data");
+    PRINT_P(" contains data");
   }
   Serial.println();
 }
@@ -175,18 +191,20 @@ static char helloWorld[32] = "Hello World!";
 
 void setup()
 {
+  digitalWrite(PIN_CE, LOW);
+  digitalWrite(PIN_SS, HIGH);
+  digitalWrite(PIN_MOSI, LOW);
+  digitalWrite(PIN_SCK, LOW);
+  
   pinMode(PIN_CE, OUTPUT);
   pinMode(PIN_SS, OUTPUT);
-  pinMode(PIN_CS, OUTPUT);
   pinMode(PIN_MOSI, OUTPUT);
   pinMode(PIN_SCK, OUTPUT);
-  
   pinMode(PIN_IRQ, INPUT); 
   pinMode(PIN_MISO, INPUT);
   
   digitalWrite(PIN_CE, LOW);
   digitalWrite(PIN_SS, HIGH);
-  digitalWrite(PIN_CS, HIGH);
   digitalWrite(PIN_MOSI, LOW);
   digitalWrite(PIN_SCK, LOW);
   
@@ -194,16 +212,17 @@ void setup()
   SPI.setBitOrder(MSBFIRST);
   SPI.setClockDivider(SPI_CLOCK_DIV4);
   
-  Serial.begin(9600);
+  Serial.begin(115200);
   
   // wait for module to boot up (generously)
   delay(250);
   
-  Serial.println("Initializing module");
-  initRX(PIN_CS);
-  flushRX(PIN_CS);
-  dump_regs(PIN_CS);
-  Serial.println("Module init completed");
+  PRINTLN_P("Initializing module");
+  initRX(PIN_SS);
+  flushRX(PIN_SS);
+  dump_regs(PIN_SS);
+  writeReg(PIN_SS, REG_STATUS, 0x70);
+  PRINTLN_P("Module init completed");
     
   // wait for power on (generously)
   delay(250);
@@ -213,13 +232,14 @@ void setup()
 
 void loop()
 {
+  // TODO: Should we drop the CE pin in here somewhere?
   if(digitalRead(PIN_IRQ) != HIGH)
   {    
     Serial.println("IRQ asserted");
     //uint8_t statusRegister;
     uint8_t fifoStatus;
     //readReg(CS_1, REG_STATUS, 1, &statusRegister);
-    readReg(PIN_CS, REG_FIFO_STATUS, 1, &fifoStatus);
+    readReg(PIN_SS, REG_FIFO_STATUS, 1, &fifoStatus);
     
     bool read = false;
     
@@ -228,26 +248,27 @@ void loop()
     {
       read = true;
       char buf[32];
-      receive(PIN_CS, PIN_CE, sizeof(buf), (uint8_t*)buf);
+      receive(PIN_SS, PIN_CE, sizeof(buf), (uint8_t*)buf);
       Serial.println("Received data: ");
       for(int i=0; i<32; ++i)
         Serial.write(buf[i]);
       Serial.println();
       
-      readReg(PIN_CS, REG_FIFO_STATUS, 1, &fifoStatus);
+      readReg(PIN_SS, REG_FIFO_STATUS, 1, &fifoStatus);
     }
     
-    // clear status register
-    writeReg(PIN_CS, REG_STATUS, 0x70);
+    // clear status register to clear the assert
+    writeReg(PIN_SS, REG_STATUS, 0x70);
     
-    digitalWrite(13, HIGH);
-    delay(100);
-    digitalWrite(13, LOW);
+    if(read) 
+    {
+      digitalWrite(13, HIGH);
+      delay(100);
+      digitalWrite(13, LOW);
+    }
   }
 }
-
-void dump_regs(uint8_t pin) { }
-#if 0
+  
   
 void dump_regs(uint8_t pin)
 {
@@ -258,158 +279,158 @@ void dump_regs(uint8_t pin)
   
   if((val & 0x70) != 0x70)
   {
-    Serial.print("IRQ enabled for ");
+    PRINT_P("IRQ enabled for ");
     
     bool first = true;
     
     if(!(val & (1<<6)))
     {
       if(!first)
-        Serial.print(", ");
+        PRINT_P(", ");
       else
         first = false;
-      Serial.print("RX_DR");
+      PRINT_P("RX_DR");
     }
     
     if(!(val & (1<<5)))
     {
       if(!first)
-        Serial.print(", ");
+        PRINT_P(", ");
       else
         first = false;
-      Serial.print("TX_DS");
+      PRINT_P("TX_DS");
     }
     
     if(!(val & (1<<4)))
     {
       if(!first)
-        Serial.print(", ");
+        PRINT_P(", ");
       else
         first = false;
-      Serial.print("MAX_RT");
+      PRINT_P("MAX_RT");
     }
     
-    Serial.print("; ");
+    PRINT_P("; ");
   }
   else
   {
-    Serial.print("IRQs all disabled; ");
+    PRINT_P("IRQs all disabled; ");
   }
   
   if(val & (1<<3))
-    Serial.print("CRC Enabled; ");
+    PRINT_P("CRC Enabled; ");
   else
-    Serial.print("CRC Disabled; ");
+    PRINT_P("CRC Disabled; ");
     
   if(val & (1<<2))
-    Serial.print("2 byte CRC; ");
+    PRINT_P("2 byte CRC; ");
   else
-    Serial.print("1 byte CRC; ");
+    PRINT_P("1 byte CRC; ");
     
   if(val & (1<<1))
-    Serial.print("Power Up; ");
+    PRINT_P("Power Up; ");
   else
-    Serial.print("Power Down; ");
+    PRINT_P("Power Down; ");
     
   if(val & (1<<0))
-    Serial.print("Receiver");
+    PRINT_P("Receiver");
   else
-    Serial.print("Transmitter");
+    PRINT_P("Transmitter");
     
   Serial.println();
   // END CONFIG
   
   // BEGIN SETUP_AW  
   readReg(pin, 0x03, 1, &val);
-  Serial.print("Address width: ");
+  PRINT_P("Address width: ");
   const char* aw;
   switch(val & 0x3)
   {    
-    case 0: aw = "!!!Illegal!!!"; break;
-    case 1: aw = "3 bytes"; break;
-    case 2: aw = "4 bytes"; break;
-    case 3: aw = "5 bytes"; break;
+    case 0: aw = PSTR("!!!Illegal!!!"); break;
+    case 1: aw = PSTR("3 bytes"); break;
+    case 2: aw = PSTR("4 bytes"); break;
+    case 3: aw = PSTR("5 bytes"); break;
   }
-  Serial.println(aw);
+  println_p(aw);
   // END SETUP_AW register
   
   // BEGIN SETUP_RETR register  
   readReg(pin, 0x04, 1, &val);
-  Serial.print("Auto Retransmit Interval: ");
+  PRINT_P("Auto Retransmit Interval: ");
   uint16_t interval = (val & 0xF0) >> 4;
   interval += 1;
   interval *= 250;
   Serial.print(interval, DEC);
-  Serial.print(" us; Auto Retransmit Count: ");
+  PRINT_P(" us; Auto Retransmit Count: ");
   Serial.println((val & 0x0F));
   // END SETUP_RETR register
   
   // BEGIN RF_CH register  
   readReg(pin, 0x05, 1, &val);
-  Serial.print("RF Channel: ");
+  PRINT_P("RF Channel: ");
   int ch = val & 0x7F;
   Serial.print(ch);
-  Serial.print(" (2.");  
+  PRINT_P(" (2.");  
   Serial.print(ch+400, DEC);
   // TODO: Calculate GHz value
-  Serial.println(" GHz)");
+  PRINTLN_P(" GHz)");
   // END RF_CH register
   
   // BEGIN RF_SETUP register  
   readReg(pin, 0x06, 1, &val);
-  Serial.print("Continuous Carrier TX ");
+  PRINT_P("Continuous Carrier TX ");
   Serial.print(val & (1<<7) ? "ON" : "OFF");
-  Serial.print("; Force PLL Lock ");
+  PRINT_P("; Force PLL Lock ");
   Serial.print(val & (1<<4) ? "ON" : "OFF");
-  Serial.print("; ");
+  PRINT_P("; ");
   uint8_t rf_dr_low = (val>>5)&0x1;
   uint8_t rf_dr_high = (val>>3)&0x1;
   switch((rf_dr_low<<1) | rf_dr_high) {
-    case 0: Serial.print("1 Mbps"); break;
-    case 1: Serial.print("2 Mbps"); break;
-    case 2: Serial.print("250 kbps"); break;
-    case 3: Serial.print("**RESERVED RATE**"); break;
+    case 0: PRINT_P("1 Mbps"); break;
+    case 1: PRINT_P("2 Mbps"); break;
+    case 2: PRINT_P("250 kbps"); break;
+    case 3: PRINT_P("**RESERVED RATE**"); break;
   }
-  Serial.print("; Transmit Power ");
+  PRINT_P("; Transmit Power ");
   uint8_t rf_pwr = (val>>1)&0x3;
   switch(rf_pwr) {
-    case 0: Serial.print("-18 dBm");
-    case 1: Serial.print("-12 dBm");
-    case 2: Serial.print("-6 dBm");
-    case 3: Serial.print("0 dBm");
+    case 0: PRINT_P("-18 dBm");
+    case 1: PRINT_P("-12 dBm");
+    case 2: PRINT_P("-6 dBm");
+    case 3: PRINT_P("0 dBm");
   }
   Serial.println();
   // END RF_SETUP register
   
   // Begin STATUS register
   readReg(pin, 0x07, 1, &val);
-  Serial.print("");
-  Serial.print((val & (1<<6)) ? "Data Ready; " : "No Data Ready; ");
-  Serial.print((val & (1<<5)) ? "Data Sent; " : "Data Not Sent; ");
-  Serial.print((val & (1<<4)) ? "Max Retransmits Hit; " : "Max Retransmits Not Hit; ");
+  PRINT_P("");
+  print_p((val & (1<<6)) ? PSTR("Data Ready; ") : PSTR("No Data Ready; "));
+  print_p((val & (1<<5)) ? PSTR("Data Sent; ") : PSTR("Data Not Sent; "));
+  print_p((val & (1<<4)) ? PSTR("Max Retransmits Hit; ") : PSTR("Max Retransmits Not Hit; "));
   uint8_t plno = (val>>1) & 0x7;
   switch(plno)
   {
     case 0: case 1: case 2: case 3: case 4: case 5:
-      Serial.print("RX FIFO has data from pipe ");
+      PRINT_P("RX FIFO has data from pipe ");
       Serial.print(plno, DEC);
-      Serial.print("; ");
+      PRINT_P("; ");
       break;
     case 6:
-      Serial.print("INVALID RX_P_NO!!! ");
+      PRINT_P("INVALID RX_P_NO!!! ");
       break;
     case 7:
-      Serial.print("RX FIFO empty; ");
+      PRINT_P("RX FIFO empty; ");
       break;
   }
-  Serial.println((val & 0x1) ? "TX FIFO full" : "TX FIFO not full");
+  println_p((val & 0x1) ? PSTR("TX FIFO full") : PSTR("TX FIFO not full"));
   // End STATUS register
   
   // Begin OBSERVE_TX
   readReg(pin, 0x08, 1, &val);
-  Serial.print("Packets lost: ");
+  PRINT_P("Packets lost: ");
   Serial.print((val >> 4) & 0xF, DEC);
-  Serial.print("; Retransmitted: ");
+  PRINT_P("; Retransmitted: ");
   Serial.println(val & 0xF, DEC);  
   // End OBSERVE_TX
   
@@ -428,7 +449,7 @@ void dump_regs(uint8_t pin)
   readReg(pin, 0x1C, 1, &dynamicPayloadLen);
   
   // EN_AA, EN_RXADDR, RX_ADDR_Pn, RX_PW_Pn, DYNPD
-  Serial.println("RX Pipes:");
+  PRINTLN_P("RX Pipes:");
   for(uint8_t i=0; i<6; ++i)
   {
     uint8_t len = 5;
@@ -436,41 +457,42 @@ void dump_regs(uint8_t pin)
       len = 1;
     readReg(pin, 0x0A + i, len, addr);
     Serial.print(i,DEC);
-    Serial.print(": ");
+    PRINT_P(": ");
     print_addr(addr);
-    Serial.print(autoAck & (1<<i) ? " AA" : "   ");
-    Serial.print(enableAddr & (1<<i) ? " RX" : "   ");
-    Serial.print(dynamicPayloadLen & (1<<i) ? " DPL" : "    ");
+    // TODO: PRINT_P
+    print_p(autoAck & (1<<i) ? PSTR(" AA") : PSTR("   "));
+    print_p(enableAddr & (1<<i) ? PSTR(" RX") : PSTR("   "));
+    print_p(dynamicPayloadLen & (1<<i) ? PSTR(" DPL") : PSTR("    "));
     
     uint8_t pw;
     readReg(pin, 0x11 + i, 1, &pw);
     
-    Serial.print("PW=");
+    PRINT_P("PW=");
     Serial.print(pw,DEC);
-    Serial.println(" bytes");
+    PRINTLN_P(" bytes");
   }
   
   // Begin TX_ADDR
   readReg(pin, 0x10, 5, addr);
-  Serial.print("TX Addr: ");
+  PRINT_P("TX Addr: ");
   print_addr(addr);
   Serial.println();
   // End TX_ADDR
   
   // Begin FIFO_STATUS
   readReg(pin, 0x17, 1, &val);
-  Serial.print((val & (1<<6)) ? "Reusing TX payload; " : "Not reusing TX payload; ");
-  Serial.print((val & (1<<5)) ? "TX FIFO full; " : "TX FIFO not full; ");
-  Serial.print((val & (1<<4)) ? "TX FIFO empty; " : "TX FIFO nonempty; ");
-  Serial.print((val & (1<<1)) ? "RX FIFO full; " : "RX FIFO not full; ");
-  Serial.println((val & (1<<0)) ? "RX FIFO empty" : "RX FIFO nonempty");
+  print_p((val & (1<<6)) ? PSTR("Reusing TX payload; ") : PSTR("Not reusing TX payload; "));
+  print_p((val & (1<<5)) ? PSTR("TX FIFO full; ") : PSTR("TX FIFO not full; "));
+  print_p((val & (1<<4)) ? PSTR("TX FIFO empty; ") : PSTR("TX FIFO nonempty; "));
+  print_p((val & (1<<1)) ? PSTR("RX FIFO full; ") : PSTR("RX FIFO not full; "));
+  println_p((val & (1<<0)) ? PSTR("RX FIFO empty") : PSTR("RX FIFO nonempty"));
   // End FIFO_STATUS
   
   // Begin FEATURE
   readReg(pin, 0x1D, 1, &val);
-  Serial.print((val & (1<<2)) ? "Dynamic Payload Length Enabled; " : "Dynamic Payload Length Disabled; ");  
-  Serial.print((val & (1<<1)) ? "ACK Payload Enabled; " : "ACK Payload Disabled; ");
-  Serial.println((val & (1<<0)) ? "Dynamic ACK enabled" : "Dynamic ACK Disabled");
+  print_p((val & (1<<2)) ? PSTR("Dynamic Payload Length Enabled; ") : PSTR("Dynamic Payload Length Disabled; "));  
+  print_p((val & (1<<1)) ? PSTR("ACK Payload Enabled; ") : PSTR("ACK Payload Disabled; "));
+  println_p((val & (1<<0)) ? PSTR("Dynamic ACK enabled") : PSTR("Dynamic ACK Disabled"));
   // End FEATURE
   
   Serial.println();
@@ -478,11 +500,10 @@ void dump_regs(uint8_t pin)
 
 void print_addr(uint8_t* addr)
 {
-  Serial.print("0x");
+  PRINT_P("0x");
   for(int i = 4; i >= 0; --i) {
     if(addr[i] < 10)
-      Serial.print("0");
+      PRINT_P("0");
     Serial.print(addr[i], HEX);
   }  
 }
-#endif
